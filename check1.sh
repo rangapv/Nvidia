5faff175dcd4c4ad323d7dd076ff616ea09a6516
rangapv <rangapv@yahoo.com>
2025-05-05 11:18:12 +0000

    Updated with package checks

--- check1.sh
+++ check1.sh
@@ -40,7 +40,44 @@ fi
 }
 
 
+chkifinsta() {
+
+cmd1=("$@")
+tcmd="$#"
+insdep=0
+
+echo "the total depedency to check is $tcmd"
+
+for i in "${cmd1[@]}"
+do
+
+wc=`which $i`
+wcs="$?"
+
+if [[ ( $wcs == "0" ) ]]
+then
+    echo "\"$i\" is installed proceeding with other checks"
+else
+    echo "\"$i\"  is not installed .pls install it and then re-run this script for other tasks"
+    insdep=1 
+fi
+
+done
+
+if (( $insdep == 1 ))
+then
+   echo "Install all the dependencies and proceed after, exiting now"
+   exit
+else
+   echo "All the dependecy \" ${cmd1[@]} \" are installed"
+fi
+
+}
+
+
 chip_onboard
 
 nvd_smi
+
+chkifinsta python3 pip3 gcc
 
