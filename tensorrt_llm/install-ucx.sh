f4ae71080fa93b155607f9486cf0314ffd33f51f
rangapv <rangapv@yahoo.com>
2025-11-12 06:21:18 +0000

    install ucx and check the version info

---
+++ tensorrt_llm/install-ucx.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#author:rangapv@yahoo.com
+#07-11-25
+
+source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh) > /dev/null 2>&1
+
+ins_ucx() {
+uc1=`git clone https://github.com/openucx/ucx.git`
+uc11=`sudo mkdir /usr/local/bin/ucx`
+uc12=`./ucx/autogen.sh`
+uc21=`./ucx/contrib/configure-release --prefix=/usr/local/bin/ucx`
+uc3=`/usr/local/bin/ucx/make -j8`
+uc5=`/usr/local/bin/ucx/sudo make install`
+uc51=`"export PATH=:/usr/local/bin/ucx/bin:$PATH" >> ~/.bashrc`
+`source ~/.bashrc`
+uc6=`ucx_info -v`
+#openucx --version
+echo "uc5 is $uc6"
+
+}
+
+
+ins_ucx
