296557ed735a129d21099d8adf644c3ea997e1cc
rangapv <rangapv@yahoo.com>
2026-01-01 06:45:26 +0000

    fixed home path

--- tensorrt_llm/install-ucx.sh
+++ tensorrt_llm/install-ucx.sh
@@ -3,14 +3,14 @@
 #07-11-25
 
 source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh) > /dev/null 2>&1
-
+HOMEDIR="$PWD"
 ins_ucx() {
 uc1=`git clone https://github.com/openucx/ucx.git`
 uc11=`sudo mkdir /usr/local/bin/ucx`
-uc12=`./ucx/autogen.sh`
-uc21=`./ucx/contrib/configure-release --prefix=/usr/local/bin/ucx`
-uc3=`./ucx/make -j8`
-uc5=`/ucx/sudo make install`
+uc12=`$HOMEDIR/ucx/autogen.sh`
+uc21=`$HOMEDIR/ucx/contrib/configure-release --prefix=/usr/local/bin/ucx`
+uc3=`$HOMEDIR/ucx/make -j8`
+uc5=`$HOMEDIR/ucx/sudo make install`
 uc51=`"export PATH=:/usr/local/bin/ucx/bin:$PATH" >> ~/.bashrc`
 `source ~/.bashrc`
 uc6=`ucx_info -v`
